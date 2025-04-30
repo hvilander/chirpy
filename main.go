6cac481c274c095630f2d7230f39030ba0e51544
Harold <harold.vilander@gmail.com>
2025-04-30 13:24:34 -0500

    middleware

--- main.go
+++ main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
 const PORT_STR = ":8080"
 
-func handleHealthz(w http.ResponseWriter, req *http.Request) {
+type apiConfig struct {
+	hitCount atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.hitCount.Add(1)
+		next.ServeHTTP(w, req)
+	})
 
 }
 
@@ -19,12 +28,24 @@ func main() {
 		Addr:    ":8080",
 		Handler: mux,
 	}
+	apiConfig := apiConfig{}
 
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", apiConfig.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
+	})
+
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte(fmt.Sprintf("Hits: %d", apiConfig.hitCount.Load())))
+	})
+	mux.HandleFunc("/reset", func(w http.ResponseWriter, req *http.Request) {
+		w.WriteHeader(200)
+		apiConfig.hitCount.Store(0)
 	})
 
 	fmt.Println("Server starting on", "http://localhost"+PORT_STR)
